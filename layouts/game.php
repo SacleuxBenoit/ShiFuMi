7658de3b1e1c4e93fccce88661fbc6e162a49ebe
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-05-20 12:28:30 +0200

    create 2 div, for player && bot + 3 inputs in each div :arrow_right: layouts/game.php

--- layouts/game.php
+++ layouts/game.php
@@ -1,12 +1,35 @@
+<?php
+include('../login_database.php');
+include('../database/connection_database.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>SHIFUMI - game</title>
 </head>
 <body>
-    
+    <div id="player">
+        <p>PlacerHolder image</p>
+        
+        <p>
+            <button>SHI</button>
+            <button>FU</button>
+            <button>MI</button>
+        </p>
+    </div>
+
+    <div id="bot">
+        <p>PlacerHolder image</p>
+        
+        <p>
+            <button>SHI</button>
+            <button>FU</button>
+            <button>MI</button>
+        </p>
+    </div>
+
 </body>
 </html>
